9930b7389a52ce1624b74f34422a6a28f7bf54f1
Shaun Jesus Barreto <shaun.barreto@zapcg.com>
2024-01-29 09:29:17 +0530

    uploaded quote generator

---
+++ index.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    getQuote();
+});
+
+async function getQuote(){
+    const response = await fetch("https://api.quotable.io/random");
+    const data = await response.json();
+    document.getElementById("quoteText").innerHTML ='"' + data.content + '"';
+    document.getElementById("quoteAuthor").innerHTML = '-' + data.author;
+
+
+}
